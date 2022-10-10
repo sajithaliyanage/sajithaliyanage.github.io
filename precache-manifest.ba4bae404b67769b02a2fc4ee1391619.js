b0951c40d5efddab86ceaabce5beda542eaa8434
sajithaliyanage <sajithaliyanage@gmail.com>
2022-10-10 15:32:09 +0300

    Updates

---
+++ precache-manifest.ba4bae404b67769b02a2fc4ee1391619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b143615eb11046029848292d8bec4853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33547689dfa0720418d6",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8bab9697512524bee431",
+    "url": "/static/css/main.70c7bf1a.chunk.css"
+  },
+  {
+    "revision": "33547689dfa0720418d6",
+    "url": "/static/js/2.30ddd0fb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.30ddd0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bab9697512524bee431",
+    "url": "/static/js/main.53fe4bab.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
